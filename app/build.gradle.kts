9dbc3aaa6b593924e185575166c98669973cc89f
GouravShDev <gourav0sharma1@gmail.com>
2024-01-28 13:58:21 +0530

    feat: add animated splash screen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.gouravsh.dot"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.gouravsh.dot"
@@ -50,7 +50,7 @@ android {
 }
 
 dependencies {
-
+    implementation("androidx.core:core-splashscreen:1.0.0")
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
